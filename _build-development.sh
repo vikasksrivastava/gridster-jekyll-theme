818420bfbcbd67d503d4e4a03a114a7af2985925
KolenCheung <christian.kolen@gmail.com>
2016-05-17 15:45:24 -0700

    Cleanup scripts

--- _build-development.sh
+++ _build-development.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
-open http://127.0.0.1:4000/gridster-jekyll-theme/
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+python -mwebbrowser http://127.0.0.1:4000/gridster-jekyll-theme/
 bundle exec jekyll serve --profile --incremental --drafts
